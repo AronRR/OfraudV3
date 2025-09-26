2e36f95c9f2775d179715b937723403165483952
AronRR <A01666725@tec.mx>
2025-09-25 19:48:03 -0600

    Bueno

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -12,7 +12,7 @@ export class DbService implements OnModuleInit, OnModuleDestroy{
             host: 'localhost',
             user: 'root',
             password: 'Aroneflaco12&',
-            database: '4521_demo',
+            database: '451_demo',
         })
     }
     onModuleDestroy() {
